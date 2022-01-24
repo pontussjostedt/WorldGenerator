e1d00bba142ac870ff504ba0dc66679e1e1f0321
OlofBen <mejladr@plats.se>
2022-01-24 09:17:12 +0100

    Code layout

--- src/main/scala/World/World.scala
+++ src/main/scala/World/World.scala
@@ -3,13 +3,15 @@ import WorldObject.WorldObject
 import scala.collection.mutable.Map
 object World {
   val underlying = Map.empty[(Int, Int), WorldObject];
+  def deafult(pos:(Int, Int)) = WorldObject.creatEmpty(pos);
   
-
-  def get(pos:(Int, Int)) = underlying.getOrElse(pos, WorldObject.creatEmpty(pos));
+  def get(pos:(Int, Int)) = underlying.getOrElse(pos, deafult(pos));
 
   def remove(pos:(Int, Int)) = underlying.remove(pos);
 
   def uppdate(pos:(Int,Int), value:WorldObject) = underlying.update(pos, WorldObject);
 
   def set(pos:(Int,Int), value:WorldObject) = underlying.put(pos, value);
+
+  def apply(pos:(Int, Int)) = get(pos);
 }

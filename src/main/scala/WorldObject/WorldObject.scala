99e8b4dfc1b8a40109154d6a2b3060596513fa65
OlofBen <mejladr@plats.se>
2022-01-24 07:14:18 +0100

    Implemented CRUD for world

--- src/main/scala/WorldObject/WorldObject.scala
+++ src/main/scala/WorldObject/WorldObject.scala
@@ -1,5 +1,13 @@
 package WorldObject
 
-abstract class WorldObject {
+abstract class WorldObject(var pos:(Int, Int)) {
   
 }
+
+class Empty(pos:(Int, Int) = (0,0))
+
+object WorldObject {
+    def creatEmpty(pos:(Int, Int) = (0,0)) = Empty(pos)
+}
+
+
